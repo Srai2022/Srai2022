77c3718db9a7e4e3e8701289ab7a46a45e28ecb8
Srai2022 <117318322+Srai2022@users.noreply.github.com>
2024-01-08 18:51:56 -0600

    Create audio.js

---
+++ audio.js
@@ -0,0 +1,13 @@
+
+  const audio = document.getElementById("myAudio"); 
+  
+  function playAudio() { 
+    audio.play(); 
+  } 
+  
+  function pauseAudio() { 
+    audio.pause(); 
+  } 
+  play.addEventListener("click",playAudio);
+
+
